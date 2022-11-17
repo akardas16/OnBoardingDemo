076567e86d185235b87b9d8334ab081c949605b3
akardas16 <krdsabdllh53@gmail.com>
2022-11-17 15:48:38 +0100

    updated

--- app/src/main/java/com/akardas/onboardingdemo/segmentedProgressBar/ContextExtensions.kt
+++
@@ -1,5 +0,0 @@
-package com.akardas.onboardingdemo.segmentedProgressBar
-
-import android.content.Context
-
-fun Context.dp(valueInDp: Int): Int = (valueInDp * resources.displayMetrics.density).toInt()
